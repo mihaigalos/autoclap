71e59d005f3f3b0886f833565d598ad999729453
Mihai Galos <mihaigalos@gmail.com>
2022-01-27 22:18:56 +0100

    Working implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,69 @@
+use clap::{App, Arg};
+
+pub fn autoclap() -> clap::App<'static> {
+    let repo = env!("CARGO_PKG_REPOSITORY");
+    if repo.contains("github") {
+        return App::new(concat!(
+            env!("CARGO_CRATE_NAME"),
+            " ",
+            env!("CARGO_PKG_VERSION"),
+            " :: ",
+            concat!(
+                env!("CARGO_PKG_REPOSITORY"),
+                "/releases/tag/",
+                env!("CARGO_PKG_VERSION"),
+            )
+        ))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::new("debug")
+                .long("debug")
+                .short('d')
+                .help("Print raw data used in statistics."),
+        );
+    } else if repo.contains("gitlab") {
+        return App::new(concat!(
+            env!("CARGO_CRATE_NAME"),
+            " ",
+            env!("CARGO_PKG_VERSION"),
+            " :: ",
+            concat!(
+                env!("CARGO_PKG_REPOSITORY"),
+                "/-/releases/",
+                env!("CARGO_PKG_VERSION"),
+            )
+        ))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::new("debug")
+                .long("debug")
+                .short('d')
+                .help("Print raw data used in statistics."),
+        );
+    } else {
+        panic!("Cannot determine repository.");
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_autoclap_name_works_when_typical() {
+        let app = autoclap();
+        assert_eq!(
+            app.get_name(),
+            "autoclap 0.0.2 :: https://github.com/mihaigalos/autoclap/releases/tag/0.0.2"
+        );
+    }
+    #[test]
+    fn test_autoclap_author_works_when_typical() {
+        let app = autoclap();
+        assert_eq!(
+            app.get_about().unwrap(),
+            "👏 Auto-propagate Cargo.toml infos (name, version, author, repo) into app."
+        );
     }
 }
