34b9620c21402d9836d2c6275de258cc62f13741
Mihai Galos <mihaigalos@gmail.com>
2022-10-03 19:16:46 +0200

    Bump version to 0.2.10

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,29 @@
 #[macro_export]
 macro_rules! autoclap {
     () => {
-        Command::new(format!(
-            "{}{}{}{}{}",
-            env!("CARGO_PKG_NAME"),
-            " ",
-            env!("CARGO_PKG_VERSION"),
-            " :: ",
-            format!(
-                "{}{}{}",
-                env!("CARGO_PKG_REPOSITORY"),
-                "/releases/tag/",
+        Command::new(format!("{}", env!("CARGO_PKG_NAME")))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about(format!(
+                "{} {}{}{}\n{}",
+                env!("CARGO_PKG_NAME"),
                 env!("CARGO_PKG_VERSION"),
+                " :: ",
+                format!(
+                    "{}{}{}",
+                    env!("CARGO_PKG_REPOSITORY"),
+                    "/releases/tag/",
+                    env!("CARGO_PKG_VERSION"),
+                ),
+                env!("CARGO_PKG_DESCRIPTION")
+            ))
+            .arg(
+                Arg::new("debug")
+                    .long("debug")
+                    .short('D')
+                    .action(ArgAction::SetTrue)
+                    .help("Print raw data used internally.")
+                    .required(false),
             )
-        ))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(
-            Arg::new("debug")
-                .long("debug")
-                .short('D')
-                .action(ArgAction::SetTrue)
-                .help("Print raw data used internally.")
-                .required(false),
-        )
     };
 }
 
@@ -42,11 +42,7 @@ mod tests {
     }
     #[test]
     fn test_autoclap_name_works_when_typical() {
-        let version = grep_version();
-        let expected = "autoclap ".to_string()
-            + &version
-            + " :: https://github.com/mihaigalos/autoclap/releases/tag/"
-            + &version;
+        let expected = "autoclap".to_string();
         let app = autoclap!();
 
         let actual = app.get_name();
@@ -55,10 +51,18 @@ mod tests {
     }
     #[test]
     fn test_autoclap_description_works_when_typical() {
-        let expected = "👏 Auto-propagate Cargo.toml infos (name, version, author, repo) into app.";
+        let version = grep_version();
+        let expected = "autoclap ".to_string()
+            + &version
+            + " :: https://github.com/mihaigalos/autoclap/releases/tag/"
+            + &version
+            + "\n"
+            + "👏 Auto-propagate Cargo.toml infos (name, version, author, repo) into app.";
         let app = autoclap!();
 
         let actual = app.get_about().unwrap().to_string();
+
+        println!("{}", actual);
 
         assert_eq!(actual, expected);
     }
